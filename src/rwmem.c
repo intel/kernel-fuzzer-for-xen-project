b4b9fd8e81d5d90a7552f5f9e2af2d5c23cfbb07
Tamas K Lengyel <tamas@tklengyel.com>
2021-02-18 09:58:36 -0500

    Update rwmem

--- src/rwmem.c
+++ src/rwmem.c
@@ -24,10 +24,14 @@ static void usage(void)
     printf("\t --write <address>\n");
     printf("\t --file <input/output file>\n");
     printf("\t --limit <input/output limit>\n");
+
+    printf("Optional:\n");
+    printf("\t --pagetable <address> (-1 for physical memory access)\n");
 }
 
 int main(int argc, char** argv)
 {
+    addr_t pagetable = 0;
     int c, long_index = 0;
     const struct option long_opts[] =
     {
@@ -37,9 +41,10 @@ int main(int argc, char** argv)
         {"write", required_argument, NULL, 'w'},
         {"limit", required_argument, NULL, 'L'},
         {"file", required_argument, NULL, 'f'},
+        {"pagetable", required_argument, NULL, 'p'},
         {NULL, 0, NULL, 0}
     };
-    const char* opts = "d:j:r:w:L:f:";
+    const char* opts = "d:j:r:w:L:f:p:";
     bool read = false, write = false;
     size_t limit = 0;
     addr_t address = 0;
@@ -67,6 +72,9 @@ int main(int argc, char** argv)
         case 'f':
             filepath = optarg;
             break;
+        case 'p':
+            pagetable = strtoull(optarg, NULL, 0);
+            break;
         case 'h': /* fall-through */
         default:
             usage();
@@ -85,39 +93,88 @@ int main(int argc, char** argv)
 
     access_context_t ctx = {
         .translate_mechanism = VMI_TM_PROCESS_DTB,
-        .addr = address,
-        .dtb = target_pagetable
+        .dtb = target_pagetable,
+        .addr = address
     };
 
+    if ( pagetable )
+    {
+        if ( pagetable == ~0ull )
+        {
+            ctx.translate_mechanism = VMI_TM_NONE;
+            ctx.dtb = 0;
+        }
+        else
+            ctx.dtb = pagetable;
+    }
+
     size_t fsize = 0;
-    FILE *i = NULL;
-    unsigned char *buffer = malloc(limit);
+    FILE *i = fopen(filepath, read ? "w+" : "r");
 
-    if ( !buffer )
+    if (!i)
+    {
+        printf("Failed to open %s\n", filepath);
         goto done;
-
-    if ( read )
-    {
-        i = fopen(filepath,"w+");
-
-        if ( i && VMI_SUCCESS == vmi_read(vmi, &ctx, limit, buffer, NULL) && 1 == fwrite(buffer, limit, 1, i) )
-            printf("Read operation success: %lu bytes from 0x%lx\n", limit, address);
     }
 
-    if ( write )
+    // Do large reads/writes in chunks
+    unsigned int iters = limit < VMI_PS_4KB ? 1 : limit / VMI_PS_4KB;
+    while ( iters-- )
     {
-        i = fopen(filepath,"r");
+        unsigned long access_length;
+        unsigned char buffer[VMI_PS_4KB] = {0};
 
-        if ( i && (fsize = fread(buffer, 1, limit, i)) && VMI_SUCCESS == vmi_write(vmi, &ctx, fsize, buffer, NULL) )
-            printf("Write operation success: %lu bytes to 0x%lx\n", fsize, address);
+        if ( !limit )
+            break;
+        if ( limit < VMI_PS_4KB )
+        {
+            access_length = limit;
+            limit = 0;
+        } else {
+            access_length = VMI_PS_4KB;
+            limit -= VMI_PS_4KB;
+        }
+
+        if ( read )
+        {
+            if ( VMI_SUCCESS == vmi_read(vmi, &ctx, access_length, (void*)&buffer, NULL) )
+                printf("Read operation success: %lu bytes from 0x%lx\n", access_length, ctx.addr);
+            else
+                printf("Read operation failed from 0x%lx\n", ctx.addr);
+
+            if ( 1 != fwrite(&buffer, access_length, 1, i) )
+            {
+                printf("Failed to save data in %s\n", filepath);
+                break;
+            }
+        }
+
+        if ( write )
+        {
+            if ( !(fsize = fread(&buffer, 1, access_length, i)) )
+            {
+                printf("Failed to read from %s\n", filepath);
+                break;
+            }
+
+            if ( VMI_SUCCESS == vmi_write(vmi, &ctx, fsize, (void*)&buffer, NULL) )
+                printf("Write operation success: %lu bytes to 0x%lx\n", fsize, ctx.addr);
+            else
+                printf("Write operation failed to 0x%lx\n", ctx.addr);
+
+            if ( fsize < access_length )
+            {
+                printf("Nothing left in %s to read\n", filepath);
+                break;
+            }
+        }
+
+        ctx.addr += access_length;
     }
+
+    fclose(i);
 
 done:
-    if ( i )
-        fclose(i);
-
-    free(buffer);
     vmi_destroy(vmi);
-
     return 0;
 }

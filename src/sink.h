11b44634afa0d6b9f9e9de98aaf8c5c4e5ad929a
Tamas K Lengyel <tamas.lengyel@intel.com>
2021-05-12 14:36:11 -0400

    Add KMSAN to the sink list

--- src/sink.h
+++ src/sink.h
@@ -22,6 +22,8 @@ struct sink {
  *
  * So in essence we can define the sink points as anything of interest that we would
  * want AFL to record if its reached.
+ *
+ * Sink functions listed here that are not found in the JSON will be skipped.
  */
 static struct sink sinks[] = {
     { .function = "panic" },
@@ -33,7 +35,8 @@ static struct sink sinks[] = {
      * is a legitimate page-fault that would page memory back in, it won't be able
      * to do that since there is no disk.
      */
-    { .function = "page_fault" }, // NOTE: after kernel 5.8 it is named asm_exc_page_fault
+    { .function = "page_fault" }, // <5.8 kernels
+    { .function = "asm_exc_page_fault" }, // >=5.8 kernels
 
     /*
      * Catch when KASAN starts to report an error it caught.
@@ -45,6 +48,11 @@ static struct sink sinks[] = {
      */
     { .function = "ubsan_prologue" },
 
+    /*
+     * Catch when KMSAN starts to report an error it caught.
+     * See https://github.com/google/kmsan
+     */
+    { .function = "kmsan_report" },
 
     /*
      * You can manually define sinks by virtual address or physical address as well.

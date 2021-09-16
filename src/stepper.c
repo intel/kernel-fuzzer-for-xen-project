8083fef033d5b21bc0c1409001fa2bade8cfc9dd
Tamas K Lengyel <tamas.lengyel@intel.com>
2021-09-16 10:17:48 -0400

    Add --print-regs and --print-hex options to stepper

--- src/stepper.c
+++ src/stepper.c
@@ -18,7 +18,7 @@ os_t os;
 addr_t target_pagetable;
 addr_t start_rip;
 addr_t stop_rip;
-bool loopmode, reset, stop_on_cpuid, stop_on_sysret, stop_on_breakpoint;
+bool loopmode, reset, stop_on_cpuid, stop_on_sysret, stop_on_breakpoint, print_hex, print_regs;
 int interrupted;
 unsigned long limit, count;
 xc_interface *xc;
@@ -36,13 +36,16 @@ static void usage(void)
     printf("\t --stop-on-breakpoint\n");
     printf("\t --stop-on-address <addr>\n");
     printf("\t --reset\n");
+    printf("\t --print-hex\n");
+    printf("\t --print-regs\n");
 }
 
-void print_instruction(vmi_instance_t _vmi, addr_t cr3, addr_t addr, bool *cpuid, bool *sysret, bool *breakpoint)
+static void print_instruction(vmi_instance_t _vmi, addr_t cr3, addr_t addr, bool *cpuid, bool *sysret, bool *breakpoint)
 {
     unsigned char buf[15] = {0};
     cs_insn *insn = NULL;
     size_t read = 0, insn_count = 0;
+    const char *format = print_hex ? "%-40s\t" : "%s\n";
 
     ACCESS_CONTEXT(ctx,
         .translate_mechanism = VMI_TM_PROCESS_DTB,
@@ -60,7 +63,8 @@ void print_instruction(vmi_instance_t _vmi, addr_t cr3, addr_t addr, bool *cpuid
     if ( insn_count )
     {
         gchar *str = g_strconcat(insn[0].mnemonic, " ", insn[0].op_str, NULL);
-        printf("%-40s\t", str);
+
+        printf(format, str);
         g_free(str);
 
         if ( cpuid && insn[0].id == X86_INS_CPUID )
@@ -72,12 +76,30 @@ void print_instruction(vmi_instance_t _vmi, addr_t cr3, addr_t addr, bool *cpuid
 
         cs_free(insn, insn_count);
     } else
-        printf("%-40s\t", "-");
+        printf(format, "-");
 
-    vmi_print_hex(buf, read);
+    if ( print_hex )
+        vmi_print_hex(buf, read);
 }
 
-event_response_t tracer_cb(vmi_instance_t _vmi, vmi_event_t *event)
+static void print_registers(x86_registers_t *regs)
+{
+    if ( !print_regs )
+        return;
+
+    printf("\t\t RAX: 0x%016lx \t RBX: 0x%016lx\n", regs->rax, regs->rbx);
+    printf("\t\t RBP: 0x%016lx \t RSP: 0x%016lx\n", regs->rbp, regs->rsp);
+    printf("\t\t RDI: 0x%016lx \t RSI: 0x%016lx\n", regs->rdi, regs->rsi);
+    printf("\t\t RDX: 0x%016lx \t RCX: 0x%016lx\n", regs->rdx, regs->rcx);
+    printf("\t\t R8:  0x%016lx \t R9:  0x%016lx\n", regs->r8, regs->r9);
+    printf("\t\t R10: 0x%016lx \t R11: 0x%016lx\n", regs->r10, regs->r11);
+    printf("\t\t R12: 0x%016lx \t R13: 0x%016lx\n", regs->r12, regs->r13);
+    printf("\t\t R14: 0x%016lx \t R15: 0x%016lx\n", regs->r14, regs->r15);
+    printf("\t\t CR0: 0x%016lx \t CR2: 0x%016lx\n", regs->cr0, regs->cr2);
+    printf("\t\t CR3: 0x%016lx \t CR4: 0x%016lx\n", regs->cr3, regs->cr4);
+}
+
+static event_response_t tracer_cb(vmi_instance_t _vmi, vmi_event_t *event)
 {
     bool cpuid = false;
     bool sysret = false;
@@ -86,6 +108,7 @@ event_response_t tracer_cb(vmi_instance_t _vmi, vmi_event_t *event)
     count++;
 
     print_instruction(_vmi, event->x86_regs->cr3, event->x86_regs->rip, &cpuid, &sysret, &breakpoint);
+    print_registers(event->x86_regs);
 
     if ( count >= limit || (stop_on_cpuid && cpuid) || (stop_on_sysret && sysret) || (stop_on_breakpoint && breakpoint) || event->x86_regs->rip == stop_rip )
     {
@@ -107,6 +130,8 @@ int main(int argc, char** argv)
         {"limit", required_argument, NULL, 'L'},
         {"loopmode", no_argument, NULL, 'l'},
         {"reset", no_argument, NULL, 'r'},
+        {"print-hex", no_argument, NULL, 'x'},
+        {"print-regs", no_argument, NULL, 'R'},
         {"stop-on-cpuid", no_argument, NULL, 's'},
         {"stop-on-sysret", no_argument, NULL, 't'},
         {"stop-on-breakpoint", no_argument, NULL, 'b'},
@@ -140,6 +165,12 @@ int main(int argc, char** argv)
             break;
         case 'b':
             stop_on_breakpoint = true;
+            break;
+        case 'x':
+            print_hex = true;
+            break;
+        case 'R':
+            print_regs = true;
             break;
         case 'S':
             stop_rip = strtoull(optarg, NULL, 0);
@@ -182,6 +213,7 @@ int main(int argc, char** argv)
         vmi_get_vcpuregs(vmi, &regs, 0);
 
         print_instruction(vmi, regs.x86.cr3, regs.x86.rip, NULL, NULL, NULL);
+        print_registers(&regs.x86);
 
         vmi_toggle_single_step_vcpu(vmi, &singlestep_event, 0, 1);
 
@@ -189,19 +221,25 @@ int main(int argc, char** argv)
         while ( !interrupted && VMI_SUCCESS == vmi_events_listen(vmi, 500) )
         {}
 
+        vmi_pause_vm(vmi);
+        vmi_events_listen(vmi, 0);
         vmi_toggle_single_step_vcpu(vmi, &singlestep_event, 0, 0);
 
         if ( loopmode )
         {
             vmi_pagecache_flush(vmi);
+
             if ( xc_memshr_fork_reset(xc, domid) )
             {
                 printf("Failed to reset VM, is it a fork?\n");
                 break;
             }
+
+            printf("----------------------------------------\n");
         }
 
-        printf("----------------------------------------\n");
+        vmi_resume_vm(vmi);
+
         interrupted = 0;
         count = 0;
 

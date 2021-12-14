11e85412aba7222da839177d1822ffd9d1734dbe
Tamas K Lengyel <tamas@tklengyel.com>
2021-12-14 16:25:59 +0000

    Create xenstore entry for forkvms

--- src/forkvm_main.c
+++ src/forkvm_main.c
@@ -4,11 +4,14 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <glib.h>
+#include <xenstore.h>
 #include "forkvm.h"
 
 xc_interface *xc;
 libxl_ctx *xl;
 xc_dominfo_t info;
+struct xs_handle *xsh;
 int vcpus;
 uint32_t domid, forkdomid;
 
@@ -16,7 +19,7 @@ int main(int argc, char** argv)
 {
     if ( argc < 2 )
     {
-        printf("Usage: %s <domid> [reset]\n", argv[0]);
+        printf("Usage: %s <domid>\n", argv[0]);
         return -1;
     }
 
@@ -35,6 +38,20 @@ int main(int argc, char** argv)
             printf("Fork VM id: %u\n", forkdomid);
         else
             printf("Forking VM %u failed\n", domid);
+    }
+
+    xsh = xs_open(0);
+    if ( xsh )
+    {
+        gchar *folder = g_strdup_printf("/libxl/%u", forkdomid);
+        gchar *type = g_strdup_printf("/libxl/%u/type", forkdomid);
+
+        if ( xs_mkdir(xsh, XBT_NULL, folder) )
+            xs_write(xsh, XBT_NULL, type, "hvm", 3);
+
+        xs_close(xsh);
+        g_free(folder);
+        g_free(type);
     }
 
     if ( xl )
